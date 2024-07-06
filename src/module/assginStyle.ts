749e4e1db20319906c8a69a552e3b9c79d1ea4b9
KimInteger <kjs20151240@gmail.com>
2024-07-06 22:22:58 +0900

    style_김정수 : 기본 스타일할당 변경 right값 추가

--- src/module/assginStyle.ts
+++ src/module/assginStyle.ts
@@ -3,4 +3,5 @@ export default function assginStyle(element: HTMLElement): void {
   element.style.width = '100px';
   element.style.height = '200px';
   element.style.border = '1px solid black';
+  element.style.right = '0px';
 }

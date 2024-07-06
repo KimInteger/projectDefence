163132bf9666ccaec5842ed3f93f823f4530ad25
KimInteger <kjs20151240@gmail.com>
2024-07-06 16:12:19 +0900

    feat_김정수 : btn2개를 렌더링하는 app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log('test');
+import { root } from './element';
+import { createEnd } from './module/endBtn';
+import { createStart } from './module/startBtn';
+
+document.addEventListener('DOMContentLoaded', (): void => {
+  root.appendChild(createStart());
+  root.appendChild(createEnd());
+});

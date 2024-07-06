cbe403ff71736ea5a72664ec3d4ced0113caae79
KimInteger <kjs20151240@gmail.com>
2024-07-06 13:58:56 +0900

    sav_김정수 : html로만 렌더링하기 사용불가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+class InlineScriptPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('InlineScriptPlugin', (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+        'InlineScriptPlugin',
+        (data, cb) => {
+          data.headTags = data.headTags.filter(
+            (tag) => tag.tagName !== 'script',
+          );
+          data.bodyTags = data.bodyTags.filter(
+            (tag) => tag.tagName !== 'script',
+          );
+
+          const scriptTag = {
+            tagName: 'script',
+            voidTag: false,
+            attributes: {
+              type: 'text/javascript',
+            },
+            innerHTML: compilation.assets['bundle.js'].source(),
+          };
+
+          data.bodyTags.push(scriptTag);
+
+          // Remove the original bundle.js file to avoid having it in the output
+          delete compilation.assets['bundle.js'];
+
+          cb(null, data);
+        },
+      );
+    });
+  }
+}
 
 module.exports = {
   entry: './src/test.ts',
@@ -14,9 +49,27 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new InlineScriptPlugin(),
+  ],
 };

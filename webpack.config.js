1fd080a3aabdbf81b478ae21e4bb371dd3f7debf
KimInteger <kjs20151240@gmail.com>
2024-07-06 10:43:56 +0900

    err_김정수 : ts-loader를 설치하지 않아서 실행되지 않음

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/createMonster.ts',
+  entry: './src/test.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',

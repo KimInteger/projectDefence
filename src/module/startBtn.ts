30d3d25094025be19f20f19462bf5e50e1c4a93a
KimInteger <kjs20151240@gmail.com>
2024-07-08 22:12:53 +0900

    test_김정수 : timer함수 실험

--- src/module/startBtn.ts
+++ src/module/startBtn.ts
@@ -1,10 +1,12 @@
 import makeTag from './makeTag';
 import { startWave } from './startAndEnd';
+import { timer } from './timer';
 
 export function createStart(): HTMLElement {
   const startBtn: HTMLElement = makeTag('button', 'START') as HTMLButtonElement;
   startBtn.addEventListener('click', function () {
     startWave();
+    timer();
     let interval: NodeJS.Timeout;
     interval = setInterval(() => {
       startWave();

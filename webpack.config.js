e6675de4254236de2a60383d4d5fb1650bfb7f0b
KimInteger <kjs20151240@gmail.com>
2024-07-08 21:12:30 +0900

    edit_김정수 : 코드 수정

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  mode: 'development',
   resolve: {
     extensions: ['.ts', '.js'],
   },
@@ -57,18 +56,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
     }),
     new CustomPlugin(),
   ],

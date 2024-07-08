a39cfa0d52d6a1a49fb8adcb790b182318343521
KimInteger <kjs20151240@gmail.com>
2024-07-08 21:52:39 +0900

    feat_김정수 : event에 setInterval을 줌으로써 1분에 한번씩 실행되게 변경 및 최초 1회 실행

--- src/module/startBtn.ts
+++ src/module/startBtn.ts
@@ -3,6 +3,12 @@ import { startWave } from './startAndEnd';
 
 export function createStart(): HTMLElement {
   const startBtn: HTMLElement = makeTag('button', 'START') as HTMLButtonElement;
-  startBtn.addEventListener('click', startWave);
+  startBtn.addEventListener('click', function () {
+    startWave();
+    let interval: NodeJS.Timeout;
+    interval = setInterval(() => {
+      startWave();
+    }, 60000);
+  });
   return startBtn;
 }

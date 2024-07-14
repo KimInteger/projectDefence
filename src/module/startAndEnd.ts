2099f79821a01199c4f9293cb726ce2d7475fe91
KimInteger <kjs20151240@gmail.com>
2024-07-14 14:27:49 +0900

    feat_김정수 : 함수에 map 추가

--- src/module/startAndEnd.ts
+++ src/module/startAndEnd.ts
@@ -3,16 +3,16 @@ import { Monster } from './monsterClass';
 
 let waveInterval: NodeJS.Timeout | null = null;
 
-export function startWave() {
+export function startWave(parent: HTMLElement) {
   let count: number = 1;
   waveInterval = setInterval(() => {
     if (count === 41) {
       clearInterval(waveInterval as NodeJS.Timeout);
       waveInterval = null;
     } else {
-      let monster = new Monster(`${count}`, root);
+      let monster = new Monster(`${count}`, parent);
       count++;
-      monster.move(root);
+      monster.move(parent);
     }
   }, 1000);
 }

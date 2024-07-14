21181ab5d314def545d1579ec41c590456394c90
KimInteger <kjs20151240@gmail.com>
2024-07-14 13:48:56 +0900

    feat_김정수 : move함수 수정 네모로 움직이도록

--- src/module/monsterClass.ts
+++ src/module/monsterClass.ts
@@ -3,22 +3,39 @@ import makeTag from './makeTag';
 
 export class Monster {
   element: HTMLDivElement;
+  mapWidth: number;
+  mapHeight: number;
   constructor(name: string, element: HTMLElement) {
     this.element = makeTag('div') as HTMLDivElement;
     this.element.textContent = name;
-    assginStyle(this.element);
+    this.mapWidth = element.clientWidth;
+    this.mapHeight = element.clientHeight;
+    assginStyle(this.element, allStyle.monsterStyle);
     element.appendChild(this.element);
   }
   move(element: HTMLElement): void {
     let movePoint: number = 0;
     let interval: NodeJS.Timeout;
+
     interval = setInterval((): void => {
+      if (movePoint < this.mapHeight) {
+        this.element.style.left = '0px';
+        this.element.style.top = `${movePoint}px`;
+        movePoint++;
+      } else if (movePoint < this.mapWidth + this.mapHeight) {
+        this.element.style.left = `${movePoint - this.mapHeight}px`;
+        this.element.style.top = `${this.mapHeight}px`;
+      } else if (movePoint < 2 * this.mapWidth + this.mapHeight) {
+        this.element.style.left = `${this.mapWidth}px`;
+        this.element.style.top = `${movePoint - (this.mapWidth + this.mapHeight)}px`;
+      } else if (movePoint < 2 * (this.mapWidth + this.mapHeight)) {
+        this.element.style.left = `${movePoint - 2 * this.mapWidth + this.mapHeight}px`;
+        this.element.style.top = `0px`;
+      }
+
       if (movePoint > 1000) {
         clearInterval(interval);
         element.removeChild(this.element);
-      } else {
-        this.element.style.right = `${movePoint}px`;
-        movePoint++;
       }
     }, 16);
   }

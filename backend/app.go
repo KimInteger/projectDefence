2af4c6fdda532cd1f436fce73a6e515e225fa628
김정수 <kjs20151240@gmail.com>
2024-09-20 18:06:07 +0900

    init | all
    개발환경초기화;

---
+++ backend/app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("."))
+
+	numport := 3000
+
+	http.Handle("/", fs)
+
+	log.Printf("서버 시작 : %d", numport)
+	// err 로 선언하고 ListenAndServe에서 ":PORT번호"
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("서버 시작 실패", err)
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -35,27 +35,38 @@ class CustomPlugin {
   }
 }
 
+const entryPoint = 'app';
+
 module.exports = {
-  entry: './src/app.ts',
+  entry: `./frontend/src/${entryPoint}.ts`,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', '.tsx', '.jsx', '.css'],
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // 순서 중요 css가 읽히고 style이 읽혀야 한다. script화 이후 DOM으로 넣어주기
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/, // 자스인지 확인할 거야.
         use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/, // 타스인지 확인할 거야.
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './frontend/static/index.html',
       minify: {
         collapseWhitespace: true,
         removeComments: true,

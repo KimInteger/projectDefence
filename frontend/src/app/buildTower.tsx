c043c9f03c6559af0d0adb585ef04c62c25380bf
김정수 <kjs20151240@gmail.com>
2024-09-23 16:54:56 +0900

    modi | buildTower.tsx
    공격 후 요소삭제 기능 추가

--- frontend/src/app/buildTower.tsx
+++ frontend/src/app/buildTower.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { BasicTowerProps } from '../interface/tower/basicTowerInterfac';
 
-const BuildTower: React.FC<BasicTowerProps> = ({ atk, name, dps }) => {
+const BuildTower: React.FC<BasicTowerProps> = ({
+  atk,
+  name,
+  dps,
+  targetClass,
+}) => {
   const [isAtk, setIsAtk] = useState<boolean>(false);
   const attackIntervalRef = useRef<NodeJS.Timeout | null>(null);
   let atkDps: number = 1000 / dps;
@@ -9,7 +14,23 @@ const BuildTower: React.FC<BasicTowerProps> = ({ atk, name, dps }) => {
   useEffect(() => {
     if (isAtk && !attackIntervalRef.current) {
       attackIntervalRef.current = setInterval((): void => {
-        console.log(`타워 ${name}이(가) 공격 중: ${atk} 데미지!`);
+        const target = document.querySelector(`.${targetClass}`);
+        if (target) {
+          const currentHpElement = target.querySelector('p:last-child');
+          if (currentHpElement) {
+            const currentHp = parseInt(
+              currentHpElement.textContent?.split(' : ')[1] || '0',
+            );
+            const newHp = currentHp - atk;
+            currentHpElement.textContent = `체력 : ${newHp}`;
+
+            if (newHp <= 0) {
+              target.parentElement?.remove();
+            }
+          }
+        } else {
+          console.log(`타겟 ${targetClass}을(를) 찾을 수 없습니다.`);
+        }
       }, atkDps);
     }
 
@@ -19,10 +40,12 @@ const BuildTower: React.FC<BasicTowerProps> = ({ atk, name, dps }) => {
         attackIntervalRef.current = null;
       }
     };
-  }, [isAtk, atk, atkDps]);
+  }, [isAtk, atk, atkDps, targetClass]);
 
   const attack = (): void => {
-    if (!isAtk) setIsAtk(true);
+    if (!isAtk) {
+      setIsAtk(true);
+    }
   };
 
   const handleStopAtk = () => {
@@ -30,24 +53,22 @@ const BuildTower: React.FC<BasicTowerProps> = ({ atk, name, dps }) => {
   };
 
   return (
-    <>
-      <div
-        id="tower"
-        style={{
-          width: '200px',
-          height: '200px',
-          border: '1px solid black',
-          backgroundColor: 'black',
-          color: 'white',
-        }}
-      >
-        <p>이름 : {name}</p>
-        <p>공격력 : {atk}</p>
-        <br />
-        <button onClick={attack}>공격해!</button>
-        <button onClick={handleStopAtk}>멈춰!</button>
-      </div>
-    </>
+    <div
+      id="tower"
+      style={{
+        width: '200px',
+        height: '200px',
+        border: '1px solid black',
+        backgroundColor: 'black',
+        color: 'white',
+      }}
+    >
+      <p>이름 : {name}</p>
+      <p>공격력 : {atk}</p>
+      <br />
+      <button onClick={attack}>공격해!</button>
+      <button onClick={handleStopAtk}>멈춰!</button>
+    </div>
   );
 };
 

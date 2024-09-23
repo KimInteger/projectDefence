aa36341c74f4374ef5bbc177f3035f976028b68c
김정수 <kjs20151240@gmail.com>
2024-09-23 15:15:30 +0900

    test | app.tsx
    테스트 app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,6 +3,7 @@ import SpawnMonster from './app/spawnMonster';
 import { waveInfo } from './static/dataInformation/monster/waveMonsterInformation';
 import BasicMap from './app/map/basicMap';
 import SpawnSection from './app/map/section/spawnSection';
+import TowerBuildSection from './app/map/section/towerSection';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,9 @@ const App: React.FC = () => {
         <SpawnSection spawnSpeed={1000} spawnCount={20}>
           <SpawnMonster hp={waveInfo[1].hp} name={waveInfo[1].name} />
         </SpawnSection>
+        <TowerBuildSection buildSpeed={2000}>
+          <div>Tower</div>
+        </TowerBuildSection>
       </BasicMap>
     </>
   );

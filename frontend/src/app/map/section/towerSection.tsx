e5c2b31b46a3f9b3decca616459ce7611a87a383
김정수 <kjs20151240@gmail.com>
2024-09-23 15:26:40 +0900

    modi | towerSection.tsx
    버튼 중복 클릭 방지를 위해서 useState추가

--- frontend/src/app/map/section/towerSection.tsx
+++ frontend/src/app/map/section/towerSection.tsx
@@ -6,8 +6,10 @@ const TowerBuildSection: React.FC<TowerBuildSeciontProps> = ({
   buildSpeed,
 }) => {
   const [isBuild, setIsBuild] = useState<boolean>(false);
+  const [showButton, setShowButton] = useState<boolean>(true);
 
   const handleBuildTower = (): void => {
+    setShowButton(false);
     setTimeout(() => {
       setIsBuild(true);
     }, buildSpeed);
@@ -23,13 +25,13 @@ const TowerBuildSection: React.FC<TowerBuildSeciontProps> = ({
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          border: '1px solid blakc',
+          border: '1px solid black',
           backgroundColor: 'whitesmoke',
         }}
       >
         {isBuild ? (
           children
-        ) : (
+        ) : showButton ? (
           <button
             style={{
               width: '50px',
@@ -40,6 +42,8 @@ const TowerBuildSection: React.FC<TowerBuildSeciontProps> = ({
           >
             버턴을 눌러서 타워 생성
           </button>
+        ) : (
+          <p>타워 생성중입니다.</p>
         )}
       </div>
     </>

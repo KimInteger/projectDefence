2f9f9b83132898282b41aa013daafd2659c60ce1
김정수 <kjs20151240@gmail.com>
2024-09-23 10:58:25 +0900

    create | waveMonsterInformation.ts
    일단 3웨이브만

---
+++ frontend/src/static/dataInformation/monster/waveMonsterInformation.ts
@@ -0,0 +1,7 @@
+import { BasicMonsterProp } from '../../../interface/monster/basicMonsterInterface';
+
+export const waveInfo: { [key: number]: BasicMonsterProp } = {
+  1: { hp: 100, name: 'wave1' },
+  2: { hp: 150, name: 'wave2' },
+  3: { hp: 200, name: 'wave3' },
+};

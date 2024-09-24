1f03e99246502190c60097571ff6e0b86c1d2754
김정수 <kjs20151240@gmail.com>
2024-09-24 22:12:34 +0900

    feat | spawnMonster.tsx
    useRef로 함수관리

--- frontend/src/app/spawnMonster.tsx
+++ frontend/src/app/spawnMonster.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BasicMonsterProp } from '../interface/monster/basicMonsterInterface';
+import { monsterMoveToSquare } from '../module/lib/monsterMoveToSquare';
 
 const SpawnMonster: React.FC<BasicMonsterProp> = ({ hp, name }) => {
+  const monsterRef = useRef<HTMLDivElement>(null);
+
+  // 컴포넌트가 렌더링된 후에 몬스터가 자동으로 움직이게 하기
+  useEffect(() => {
+    if (monsterRef.current) {
+      monsterMoveToSquare(monsterRef.current);
+    }
+  }, []);
+
   return (
     <div
+      ref={monsterRef}
       className={name}
       style={{
+        position: 'absolute', // 위치 이동을 위해 absolute로 설정
         width: '100px',
         height: '100px',
         border: '1px solid black',
